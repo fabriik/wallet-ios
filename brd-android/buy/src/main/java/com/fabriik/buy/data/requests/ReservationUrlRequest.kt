9d4fb5dd9f4b6f998b3e9cbd1b33ec6e093e8287
Rok Novak <rok@equaleyes.com>
2022-03-18 09:04:25 +0100

    fix: update request for Wyre

--- brd-android/buy/src/main/java/com/fabriik/buy/data/requests/ReservationUrlRequest.kt
+++ brd-android/buy/src/main/java/com/fabriik/buy/data/requests/ReservationUrlRequest.kt
@@ -3,21 +3,6 @@ package com.fabriik.buy.data.requests
 import com.squareup.moshi.Json
 
 data class ReservationUrlRequest(
-    @Json(name = "sourceAmount")
-    val sourceAmount: String,
-
-    @Json(name = "paymentMethod")
-    val paymentMethod: String,
-
-    @Json(name = "amountIncludeFees")
-    val amountIncludeFees: Boolean,
-
-    @Json(name = "sourceCurrency")
-    val sourceCurrency: String,
-
-    @Json(name = "destCurrency")
-    val destCurrency: String,
-
     @Json(name = "redirectUrl")
     val redirectUrl: String,
 
@@ -25,11 +10,5 @@ data class ReservationUrlRequest(
     val failureRedirectUrl: String,
 
     @Json(name = "referrerAccountId")
-    val referrerAccountId: String,
-
-    @Json(name = "dest")
-    val dest: String,
-
-    @Json(name = "country")
-    val country: String
+    val referrerAccountId: String
 )
