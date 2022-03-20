ad2ee50729cb59a7fa8fe88f0c92277a52b572eb
Gardner von Holt <gardner@wvh.com>
2022-03-20 04:56:11 -0500

    setting version

--- brd-ios/scripts/makebuild.sh
+++ brd-ios/scripts/makebuild.sh
@@ -58,7 +58,10 @@ else
 	scheme="BRD Internal - TestFlight"
 fi
 
-# restore build updated files
+
+echo
+echo "Restore build updated files"
+echo
 git restore breadwallet/Resources/currencies.json
 git restore breadwallet/Resources/brd-web-3.tar
 git restore breadwallet/Resources/brd-web-3-staging.tar
@@ -68,22 +71,18 @@ git restore breadwallet/Info.plist
 git restore breadwalletWidget/Info.plist
 git restore breadwalletIntentHandler/Info.plist
 
-# make sure git is clean
-# if output=$(git status --porcelain) && [ -z "$output" ]; then
-  # Working directory clean
- 	source ${script_dir}/bump_build_number.sh "$1" "$2"
- 	source ${script_dir}/download_bundles.sh
- 	source ${script_dir}/download_currencylist.sh
- 	echo
- 	echo "Making $scheme version ${mainBundleShortVersionString} build ${mainBundleVersion} ..."
-    echo
- 	source ${script_dir}/archive.sh "${scheme}"
-    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-# 	if [[ "$3" != "testnet" && "$3" != "ci" ]]; then
-# 		commit_changes
-# 	fi
-# else
-  # Uncommitted changes
-  # echo "ERROR: Uncommitted changes. Must start with a clean repo."
-  # exit 1
-# fi
+echo
+echo "Download currencies and bundles"
+echo
+source ${script_dir}/download_bundles.sh
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+source ${script_dir}/download_currencylist.sh
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+echo
+echo "Make $scheme version ${mainBundleShortVersionString} build ${mainBundleVersion} ..."
+echo
+
+source ${script_dir}/archive.sh "${scheme}"
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
