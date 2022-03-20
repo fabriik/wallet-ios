d4818863cf8008d7894d17c71a6c1e88d5e655b2
Gardner von Holt <gardner@wvh.com>
2022-03-20 13:34:37 -0500

    update to signing key on profile

--- brd-ios/scripts/export_build.sh
+++ brd-ios/scripts/export_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-archive_path="$PWD/build/"$scheme".xcarchive"
-export_path="$PWD/build"
 scheme="breadwallet"
+export_path="$PWD/build"
+archive_path="$PWD/build/"$scheme".xcarchive"
 
 # Export the build
 xcodebuild -exportArchive -archivePath "$archive_path" -exportOptionsPlist $PWD/build/exportOptions.plist -exportPath $export_path
