4e41aeaeca02ebeeb48f24c93ee39d4078b195f8
rcresnik <rok.cresnik@equaleyes.com>
2022-03-22 08:58:59 +0100

    chore: revert.

--- brd-ios/scripts/download_bundles.sh
+++ brd-ios/scripts/download_bundles.sh
@@ -6,6 +6,9 @@ source .env
 function downloadBundle() {
     bundle_name="$1-staging"
     host="${API_URL}/blocksatoshi/wallet"
+    if [[ "$2" == "prod" ]]; then
+        bundle_name = "$1"
+    fi
     echo "Downloading ${bundle_name}.tar from ${host}..."
     curl -H "Authorization: bread ${BREAD_TOKEN}" --silent --show-error --output "breadwallet/Resources/${bundle_name}.tar" https://${host}/asset/bundles/${bundle_name}/download
 }
