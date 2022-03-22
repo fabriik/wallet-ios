b9e32ce70b1ea7135ad583c43cc6f10289cc9c4b
rcresnik <rok.cresnik@equaleyes.com>
2022-03-22 08:38:20 +0100

    chore: replace currencies url.

--- brd-ios/scripts/download_currencylist.sh
+++ brd-ios/scripts/download_currencylist.sh
@@ -2,6 +2,6 @@
 # Downlaods the latest currencies list to be embedded
 source .env
 filename="currencies.json"
-host="stage2.breadwallet.com"
+host="${API_URL}/blocksatoshi/"
 echo "Downloading ${filename} from ${host}..."
-curl -H "Authorization: Bearer ${API_TOKEN}" --silent --show-error --output "breadwallet/Resources/${filename}" https://${host}/currencies
+curl -H "Authorization: bread ${BREAD_TOKEN}" --silent --show-error --output "breadwallet/Resources/${filename}" https://${host}/currencies
