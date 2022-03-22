6f3319b157934ff6844fee3ce844556c1215a85c
Kenan Mamedoff <kenan.mamedoff@equaleyes.com>
2022-03-22 09:37:38 +0000

    chore: remove gift button

--- brd-ios/breadwallet/src/Views/AccountFooterView.swift
+++ brd-ios/breadwallet/src/Views/AccountFooterView.swift
@@ -88,58 +88,25 @@ class AccountFooterView: UIView, Subscriber, Trackable {
             return UIBarButtonItem(customView: button)
         }
         
-        if currency.isGiftingEnabled {
-            let giftButton = UIButton.rounded(image: "Gift")
-            giftButton.tap = giftCallback
-            self.giftButton = giftButton
-            buttons.append(UIBarButtonItem(customView: giftButton))
-        }
-        
         let paddingWidth = C.padding[2]
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let fixedSpace = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
         fixedSpace.width = C.padding[1]
         
-        if currency.isGiftingEnabled {
-            toolbar.items = [
-                flexibleSpace,
-                buttons[0],
-                fixedSpace,
-                buttons[1],
-                fixedSpace,
-                buttons[2],
-                flexibleSpace
-            ]
-        } else {
-            toolbar.items = [
-                flexibleSpace,
-                buttons[0],
-                fixedSpace,
-                buttons[1],
-                flexibleSpace
-            ]
-        }
+        toolbar.items = [
+            flexibleSpace,
+            buttons[0],
+            fixedSpace,
+            buttons[1],
+            flexibleSpace
+        ]
         
-        if currency.isGiftingEnabled {
-            let giftingButtonWidth: CGFloat = 44.0
-            let buttonCount = 2
-            let buttonWidth = (self.bounds.width - (paddingWidth * CGFloat(buttonCount+1))) / CGFloat(buttonCount) - (giftingButtonWidth)/2.0
-            let buttonHeight = CGFloat(44.0)
-            for (index, element) in buttons.enumerated() {
-                if index != 2 {
-                    element.customView?.frame = CGRect(x: 0, y: 0, width: buttonWidth, height: buttonHeight)
-                } else {
-                    element.customView?.frame = CGRect(x: 0, y: 0, width: giftingButtonWidth, height: buttonHeight)
-                }
-            }
-        } else {
-            let buttonWidth = (self.bounds.width - (paddingWidth * CGFloat(buttons.count+1))) / CGFloat(buttons.count)
-            let buttonHeight = CGFloat(44.0)
-            buttons.forEach {
-                $0.customView?.frame = CGRect(x: 0, y: 0, width: buttonWidth, height: buttonHeight)
-            }
+        let buttonWidth = (self.bounds.width - (paddingWidth * CGFloat(buttons.count+1))) / CGFloat(buttons.count)
+        let buttonHeight = CGFloat(44.0)
+        
+        buttons.forEach {
+            $0.customView?.frame = CGRect(x: 0, y: 0, width: buttonWidth, height: buttonHeight)
         }
-
     }
 
     @objc private func send() { sendCallback?() }
