09462d6b2b02747c2209d5152cb431cf138a6e14
rcresnik <rok.cresnik@equaleyes.com>
2022-03-22 07:26:38 +0100

    chore: add auth header to curl.

--- brd-ios/scripts/download_currencylist.sh
+++ brd-ios/scripts/download_currencylist.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Downlaods the latest currencies list to be embedded
+source .env
 filename="currencies.json"
 host="stage2.breadwallet.com"
 echo "Downloading ${filename} from ${host}..."
-curl --silent --show-error --output "breadwallet/Resources/${filename}" https://${host}/currencies
+curl -H "Authorization: Bearer ${API_TOKEN}" --silent --show-error --output "breadwallet/Resources/${filename}" https://${host}/currencies
