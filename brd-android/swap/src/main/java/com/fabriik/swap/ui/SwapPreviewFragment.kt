9a035eb2b4afc20487e589ff4d4d7bed445f0088
Rok Novak <rok@equaleyes.com>
2022-03-28 12:49:35 +0200

    feat: add preview activity

--- brd-android/swap/src/main/java/com/fabriik/swap/ui/SwapPreviewFragment.kt
+++ brd-android/swap/src/main/java/com/fabriik/swap/ui/SwapPreviewFragment.kt
@@ -6,31 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import com.fabriik.swap.R
 import com.fabriik.swap.databinding.FragmentSelectAmountBinding
+import com.fabriik.swap.databinding.FragmentSwapPreviewBinding
 
-class SelectAmountFragment : Fragment() {
+class SwapPreviewFragment : Fragment() {
 
-    private lateinit var binding: FragmentSelectAmountBinding
+    private lateinit var binding: FragmentSwapPreviewBinding
     private lateinit var viewModel: SwapViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_select_amount, container, false)
+        return inflater.inflate(R.layout.fragment_swap_preview, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentSelectAmountBinding.bind(view)
+        binding = FragmentSwapPreviewBinding.bind(view)
         viewModel = ViewModelProvider(requireActivity())
             .get(SwapViewModel::class.java)
-
-        binding.btnContinue.setOnClickListener {
-            findNavController().navigate(
-                R.id.action_swap_preview
-            )
-        }
     }
 }
