453704020a0a7894560069b087d23bd015ad261b
rcresnik <rok.cresnik@equaleyes.com>
2022-03-31 10:04:37 +0200

    fix: download scripts.

--- brd-ios/scripts/download_bundles.sh
+++ brd-ios/scripts/download_bundles.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 # Downlaods the latest app resource bundles to be embedded
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source "$SCRIPT_DIR/../.env"
+API_URL=$1
+BREAD_TOKEN=$2
 
 function downloadBundle() {
     bundle_name="$1-staging"
-    host="${API_URL}/blocksatoshi/wallet"
+    host="${API_URL}/wallet"
+    echo AAAAA
+    echo $host
+    echo AAAA
     if [[ "$2" == "prod" ]]; then
         bundle_name="$1"
     fi
     echo "Downloading ${bundle_name}.tar from ${host}..."
-    curl -H "Authorization: bread ${BREAD_TOKEN}" --silent --show-error --output "$SCRIPT_DIR/../breadwallet/Resources/${bundle_name}.tar" https://${host}/asset/bundles/${bundle_name}/download
+    curl -H "Authorization: bread ${BREAD_TOKEN}" --silent --show-error --output "$SCRIPT_DIR/../breadwallet/Resources/${bundle_name}.tar" https://${host}/assets/bundles/${bundle_name}/download
 }
 
-BREAD_TOKEN=$API_TOKEN
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 plistBuddy="/usr/libexec/PlistBuddy"
 plist="$SCRIPT_DIR/../breadwallet/Resources/AssetBundles.plist"
 bundleNames=($("${plistBuddy}" -c "print" "${plist}" | sed -e 1d -e '$d'))
--- brd-ios/scripts/download_currencylist.sh
+++ brd-ios/scripts/download_currencylist.sh
@@ -2,9 +2,9 @@
 # Downlaods the latest currencies list to be embedded
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source "$SCRIPT_DIR/../.env"
-
+API_URL=$1
+BREAD_TOKEN=$2
 filename="currencies.json"
-host="${API_URL}/blocksatoshi/"
-echo "Downloading ${filename} from ${host}..."
-curl -H "Authorization: bread ${BREAD_TOKEN}" --silent --show-error --output "$SCRIPT_DIR/../breadwallet/Resources/${filename}" https://${host}/currencies
+host="${API_URL}/wallet/currencies"
+echo "Downloading ${filename} from ${host}"
+curl -H "Authorization: bread ${BREAD_TOKEN}" --silent --show-error --output "$SCRIPT_DIR/../breadwallet/Resources/${filename}" https://${host}
