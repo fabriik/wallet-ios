1b8db7d0e8d7e134397006b6c09e27ac2b64c366
rcresnik <rok.cresnik@equaleyes.com>
2022-03-31 08:08:52 +0200

    chore: update scripts.

--- brd-ios/scripts/download_bundles.sh
+++ brd-ios/scripts/download_bundles.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # Downlaods the latest app resource bundles to be embedded
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source "$SCRIPT_DIR/../.env"
 
 function downloadBundle() {
     bundle_name="$1-staging"
     host="${API_URL}/blocksatoshi/wallet"
     if [[ "$2" == "prod" ]]; then
-        bundle_name = "$1"
+        bundle_name="$1"
     fi
     echo "Downloading ${bundle_name}.tar from ${host}..."
-    curl -H "Authorization: bread ${BREAD_TOKEN}" --silent --show-error --output "breadwallet/Resources/${bundle_name}.tar" https://${host}/asset/bundles/${bundle_name}/download
+    curl -H "Authorization: bread ${BREAD_TOKEN}" --silent --show-error --output "$SCRIPT_DIR/../breadwallet/Resources/${bundle_name}.tar" https://${host}/asset/bundles/${bundle_name}/download
 }
 
+BREAD_TOKEN=$API_TOKEN
 plistBuddy="/usr/libexec/PlistBuddy"
-plist="breadwallet/Resources/AssetBundles.plist"
-
+plist="$SCRIPT_DIR/../breadwallet/Resources/AssetBundles.plist"
 bundleNames=($("${plistBuddy}" -c "print" "${plist}" | sed -e 1d -e '$d'))
 
 for bundleName in "${bundleNames[@]}"
--- brd-ios/scripts/download_currencylist.sh
+++ brd-ios/scripts/download_currencylist.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Downlaods the latest currencies list to be embedded
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source "$SCRIPT_DIR/../.env"
+
 filename="currencies.json"
 host="${API_URL}/blocksatoshi/"
 echo "Downloading ${filename} from ${host}..."
-curl -H "Authorization: bread ${BREAD_TOKEN}" --silent --show-error --output "breadwallet/Resources/${filename}" https://${host}/currencies
+curl -H "Authorization: bread ${BREAD_TOKEN}" --silent --show-error --output "$SCRIPT_DIR/../breadwallet/Resources/${filename}" https://${host}/currencies
