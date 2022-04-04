5e17ddff8571a69acf8b4956a68d4f09c7c9a896
Rok Novak <rok@equaleyes.com>
2022-04-04 16:03:49 +0200

    fix: resolve sonarqube issues

--- brd-android/buy/src/main/java/com/fabriik/buy/data/FabriikApiConstants.kt
+++ brd-android/buy/src/main/java/com/fabriik/buy/data/FabriikApiConstants.kt
@@ -1,19 +1,14 @@
 package com.fabriik.buy.data
 
-import com.fabriik.buy.BuildConfig
-
 object FabriikApiConstants {
 
-    val HOST = when {
-        BuildConfig.DEBUG -> "one-dev.moneybutton.io/blocksatoshi"
-        else -> "one-dev.moneybutton.io/blocksatoshi" //todo: change endpoint to production
-    }
+    const val HOST = "one-dev.moneybutton.io/blocksatoshi"
 
-    private val BASE_URL = "https://$HOST"
+    private const val BASE_URL = "https://$HOST"
 
-    val HOST_WYRE_API = "$BASE_URL/wyre/"
-    val HOST_WALLET_API = "$BASE_URL/wallet"
-    val HOST_BLOCKSATOSHI_API = "$BASE_URL/blocksatoshi"
+    const val HOST_WYRE_API = "$BASE_URL/wyre/"
+    const val HOST_WALLET_API = "$BASE_URL/wallet"
+    const val HOST_BLOCKSATOSHI_API = "$BASE_URL/blocksatoshi"
 
-    val ENDPOINT_CURRENCIES = "$HOST_WALLET_API/currencies"
+    const val ENDPOINT_CURRENCIES = "$HOST_WALLET_API/currencies"
 }
