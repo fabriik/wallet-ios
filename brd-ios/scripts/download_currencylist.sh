faf47d7b366f2423ad440b2e77103cebe7144a7a
rcresnik <rok.cresnik@equaleyes.com>
2022-04-14 20:20:35 +0200

    fix: makebuild script.

--- brd-ios/scripts/download_currencylist.sh
+++ brd-ios/scripts/download_currencylist.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 API_URL=$1
 BREAD_TOKEN=$2
 filename="currencies.json"
-echo "Downloading ${filename} from ${API_URL}/wallet/currencies
-curl -H "Authorization: bread ${BREAD_TOKEN}" --silent --show-error --output "$SCRIPT_DIR/../breadwallet/Resources/${filename}" https://${API_URL}/wallet/currencies"
+echo "Downloading ${filename} from ${API_URL}/wallet/currencies"
+curl -H "Authorization: bread ${BREAD_TOKEN}" --silent --show-error --output "$SCRIPT_DIR/../breadwallet/Resources/${filename}" "https://${API_URL}/wallet/currencies"
