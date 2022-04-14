028872fecd4fa3f60704f6950596df8180ca9701
rcresnik <rok.cresnik@equaleyes.com>
2022-04-14 20:31:18 +0200

    fix: another.

--- brd-ios/scripts/download_currencylist.sh
+++ brd-ios/scripts/download_currencylist.sh
@@ -2,8 +2,8 @@
 # Downlaods the latest currencies list to be embedded
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-API_URL=$1
 BREAD_TOKEN=$2
 filename="currencies.json"
-echo "Downloading ${filename} from ${API_URL}/wallet/currencies"
-curl -H "Authorization: bread ${BREAD_TOKEN}" --silent --show-error --output "$SCRIPT_DIR/../breadwallet/Resources/${filename}" "https://${API_URL}/wallet/currencies"
+echo "Downloading $filename from $1/wallet/currencies"
+echo $1
+curl -H "Authorization: bread $BREAD_TOKEN" --silent --show-error --output "$SCRIPT_DIR/../breadwallet/Resources/$filename" "https://$1/wallet/currencies"
