ef7c10689c838147d42ba2dd788a15f138c91c0d
dabitdev <david@nordicloop.com>
2022-04-26 15:27:29 -0300

    new build

--- buildSrc/src/main/kotlin/brd/BrdRelease.kt
+++ buildSrc/src/main/kotlin/brd/BrdRelease.kt
@@ -38,10 +38,10 @@ object BrdRelease {
     private val product = ciTag?.get(1) ?: 11
 
     /** Hot fix version. Usually affected by engineering. Maximum value: 9 */
-    private val engineering = ciTag?.get(2) ?: 0
+    private val engineering = ciTag?.get(2) ?: 1
 
     /** Build version. Increase for each new build. Maximum value: 999 */
-    private val build = ciTag?.lastOrNull() ?: 26
+    private val build = ciTag?.lastOrNull() ?: 27
 
     init {
         check(marketing in 0..99)
