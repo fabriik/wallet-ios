e86fd9f2d51f3f43dfcc3af54ea7da1064224945
Dijana <angelovska.dijana@hotmail.com>
2022-05-13 09:41:40 +0200

    chore: add Roboto font family

--- brd-ios/breadwallet/src/Redesign/Theme/Fonts.swift
+++ brd-ios/breadwallet/src/Redesign/Theme/Fonts.swift
@@ -5,4 +5,20 @@
 import UIKit
 
 struct Fonts {
+    static var title_01 = ThemeManager.shared.font(for: "Degular-Sembold", size: 36)
+    static var title_02 = ThemeManager.shared.font(for: "Degular-Sembold", size: 32)
+    static var title_03 = ThemeManager.shared.font(for: "Degular-Semibold", size: 28)
+    static var title_04 = ThemeManager.shared.font(for: "Degular-Semibold", size: 24)
+    static var title_05 = ThemeManager.shared.font(for: "Degular-Semibold", size: 20)
+    static var title_06 = ThemeManager.shared.font(for: "Degular-Semibold", size: 16)
+    
+    static var subtitle_01 = ThemeManager.shared.font(for: "Roboto-Medium", size: 16)
+    static var subtitle_02 = ThemeManager.shared.font(for: "Roboto-Medium", size: 14)
+    
+    static var body_01 = ThemeManager.shared.font(for: "Roboto-Regular", size: 16)
+    static var body_02 = ThemeManager.shared.font(for: "Roboto-Regular", size: 14)
+    
+    static var caption = ThemeManager.shared.font(for: "Roboto-Regular", size: 12)
+    static var button = ThemeManager.shared.font(for: "Degular-Semibold", size: 16)
+    static var overline = ThemeManager.shared.font(for: "Roboto-Regular", size: 12)
 }
