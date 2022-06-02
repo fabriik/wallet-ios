507f892ad1b52bd610bff60fbd3032313e6a688e
Dijana <angelovska.dijana@hotmail.com>
2022-06-02 23:36:59 +0200

    chore: add verify account scene

---
+++ brd-ios/breadwallet/src/Redesign/Scenes/VerifyAccount/VerifyAccountCoordinator.swift
@@ -0,0 +1,11 @@
+//
+// Created by Equaleyes Solutions Ltd
+//
+
+import UIKit
+
+class VerifyAccountCoordinator: BaseCoordinator, VerifyAccountRoutes {
+    // MARK: - VerifyAccountRoutes
+
+    // MARK: - Aditional helpers
+}
