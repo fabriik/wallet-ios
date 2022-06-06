bfc7d75a965f65ed2919c0a05d764b5997e85955
rcresnik <rok.cresnik@equaleyes.com>
2022-06-06 13:07:38 +0200

    feat: add CheckList scene.

---
+++ brd-ios/breadwallet/src/Redesign/Scenes/CheckList/CheckListModels.swift
@@ -0,0 +1,13 @@
+//
+//  CheckListModels.swift
+//  breadwallet
+//
+//  Created by Rok on 06/06/2022.
+//
+//
+
+import UIKit
+
+enum CheckListModels {
+    
+}
---
+++ brd-ios/breadwallet/src/Redesign/Scenes/CheckList/CheckListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CheckListViewController.swift
+//  breadwallet
+//
+//  Created by Rok on 06/06/2022.
+//
+//
+
+import UIKit
+
+class CheckListViewController: BaseTableViewController<BaseCoordinator,
+                               CheckListInteractor,
+                               CheckListPresenter,
+                               CheckListStore>,
+                               CheckListResponseDisplays {
+    typealias Models = CheckListModels
+    
+    // MARK: - Overrides
+    
+    // MARK: - User Interaction
+    
+    // MARK: - CheckListResponseDisplay
+    
+    // MARK: - Additional Helpers
+}
