b8c14e5974f035c0831ad7255717aac041deca14
Dijana <angelovska.dijana@hotmail.com>
2022-06-08 09:44:08 +0200

    chore: added logo icon when no flag

--- brd-ios/breadwallet/src/Redesign/Scenes/ItemSelection/ItemSelectionInteractor.swift
+++ brd-ios/breadwallet/src/Redesign/Scenes/ItemSelection/ItemSelectionInteractor.swift
@@ -28,11 +28,12 @@ class ItemSelectionInteractor: NSObject, Interactor, ItemSelectionViewActions {
     }
     
     func search(viewAction: ItemSelectionModels.Search.ViewAction) {
-        guard let countries = dataStore?.countries else { return }
+        guard let countries = dataStore?.countries,
+        let searchText = viewAction.text else { return }
         
-//        let searchCountries = countries.filter { $0.contains(viewAction.text?.localizedUppercase ?? "") }
-//        
-//        presenter?.presentData(actionResponse: .init(item: Models.Item(searchCountries)))
+        var searchData = searchText.isEmpty ? countries : countries.filter { $0.localizedName?.contains(searchText) as? Bool ?? false }
+        
+        presenter?.presentData(actionResponse: .init(item: Models.Item(searchData)))
     }
     // MARK: - Aditional helpers
 }
