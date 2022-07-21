5cfafdc16128887b8474507e11572927992f14d1
rcresnik <rok.cresnik@equaleyes.com>
2022-07-21 08:40:32 +0200

    chore: wire up.

--- brd-ios/breadwallet/src/Redesign/Scenes/Swap/Main/SwapCoordinator.swift
+++ brd-ios/breadwallet/src/Redesign/Scenes/Swap/Main/SwapCoordinator.swift
@@ -31,34 +31,18 @@ class SwapCoordinator: BaseCoordinator, SwapRoutes {
         }
     }
     
-    func showConfirm() {
-        // TODO: pass actual data
-        guard let superview = navigationController.view else {
-            return
-        }
-        let view = WrapperPopupView<SwapConfirmationView>()
+    func showPopup<V: ViewProtocol & UIView>(with config: WrapperPopupConfiguration<V.C>?,
+                                             viewModel: WrapperPopupViewModel<V.VM>,
+                                             confirmedCallback: @escaping (() -> Void)) -> WrapperPopupView<V>? {
+        guard let superview = navigationController.view else { return nil }
+        
+        let view = WrapperPopupView<V>()
+        view.configure(with: config)
+        view.setup(with: viewModel)
+        view.confirmCallback = confirmedCallback
+        
         superview.addSubview(view)
         superview.bringSubviewToFront(view)
-        
-        view.configure(with: WrapperPopupConfiguration())
-
-        view.setup(with: .init(title: .text("Confirmation"), trailing: .init(image: "close"), buttons: [
-            .init(title: "Confirm"),
-            .init(title: "Cancel")
-        ]))
-        
-        view.callbacks = [
-            { print("confirm tapped!") },
-            { print("cancel tapped!") }
-        ]
-        
-        view.wrappedView.setup(with: .init(from: .init(title: .text("From"), value: .text("50 BSV ($2,859.00)")),
-                                   to: .init(title: .text("To"), value: .text("0.00000095 BTC ($2,857.48)")),
-                                   rate: .init(title: .text("Rate"), value: .text("1 BSV = 0.0000333 BTC")),
-                                   sendingFee: .init(title: .text("Sending Network fee\n"), value: .text("0.00023546 BSV \n($0.01)")),
-                                   receivingFee: .init(title: .text("Receiving fee\n"), value: .text("0.00023546 BSV\n($0.01)")),
-                                   totalCost: .init(title: .text("Total cost:"), value: .text("50 BSV"))))
-        view.wrappedView.configure(with: SwapConfimationConfiguration())
         
         view.snp.makeConstraints { make in
             make.edges.equalToSuperview()
@@ -70,6 +54,8 @@ class SwapCoordinator: BaseCoordinator, SwapRoutes {
         UIView.animate(withDuration: Presets.Animation.duration) {
             view.alpha = 1
         }
+        
+        return view
     }
     
     // MARK: - Aditional helpers
