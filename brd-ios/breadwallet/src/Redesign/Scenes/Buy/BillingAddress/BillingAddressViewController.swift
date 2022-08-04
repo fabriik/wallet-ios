89d214abc9cd1595115de57552a2b374020d5046
Kenan Mamedoff <kenan.mamedoff@equaleyes.com>
2022-08-04 12:53:00 +0200

    chore: ui updates

--- brd-ios/breadwallet/src/Redesign/Scenes/Buy/BillingAddress/BillingAddressViewController.swift
+++ brd-ios/breadwallet/src/Redesign/Scenes/Buy/BillingAddress/BillingAddressViewController.swift
@@ -169,7 +169,6 @@ class BillingAddressViewController: BaseTableViewController<BuyCoordinator,
             
             // TODO: add a cycle to fetch countires and pass them to the coordinator (currently the coordinator does the call)
             coordinator?.showCountrySelector { [weak self] model in
-                cell.wrappedView.animateTo(state: .filled, withAnimation: false)
                 self?.interactor?.countrySelected(viewAction: .init(code: model?.code, countryFullName: model?.name))
             }
             
