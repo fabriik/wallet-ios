e35d11a96e607e7894040a45b43742453d345ac3
Dijana <angelovska.dijana@hotmail.com>
2022-08-09 10:06:35 +0200

    chore: add supported assets that are not in the wallet

--- brd-ios/breadwallet/src/Redesign/Scenes/Swap/Main/SwapCoordinator.swift
+++ brd-ios/breadwallet/src/Redesign/Scenes/Swap/Main/SwapCoordinator.swift
@@ -25,16 +25,27 @@ class SwapCoordinator: BaseCoordinator, SwapRoutes, SwapInfoRoutes {
     func showAssetSelector(currencies: [Currency]?, supportedCurrencies: [SupportedCurrency]?, selected: ((Any?) -> Void)?) {
         let allCurrencies = CurrencyFileManager().getCurrencyMetaDataFromCache()
         
-        let supportedCurrencies = allCurrencies.filter { item in supportedCurrencies?.contains(where: { $0.name.lowercased() == item.code}) ?? false }
+        let supportedAssets = allCurrencies.filter { item in supportedCurrencies?.contains(where: { $0.name.lowercased() == item.code}) ?? false }
         
-        let data: [AssetViewModel]? = supportedCurrencies.compactMap {
+        var data: [AssetViewModel]? = currencies?.compactMap {
             return AssetViewModel(icon: $0.imageSquareBackground,
                                   title: $0.name,
                                   subtitle: $0.code,
-//                                  topRightText: HomeScreenAssetViewModel(currency: $0).tokenBalance,
-//                                  bottomRightText: HomeScreenAssetViewModel(currency: $0).fiatBalance,
-                                  isDisabled: isDisabledAsset(code: $0.code, currencies: currencies) ?? false)
+                                  topRightText: HomeScreenAssetViewModel(currency: $0).tokenBalance,
+                                  bottomRightText: HomeScreenAssetViewModel(currency: $0).fiatBalance,
+                                  isDisabled: isDisabledAsset(code: $0.code, supportedCurrencies: supportedCurrencies) ?? false)
         }
+        
+        let unsupportedAssets = supportedAssets.filter { item in !(data?.contains(where: { $0.subtitle?.lowercased() == item.code }) ?? false) }
+        
+        let disabledData: [AssetViewModel]? = unsupportedAssets.compactMap {
+            return AssetViewModel(icon: $0.imageSquareBackground,
+                                  title: $0.name,
+                                  subtitle: $0.code,
+                                  isDisabled: true)
+        }
+        
+        data?.append(contentsOf: disabledData ?? [])
         
         let sortedCurrencies = data?.sorted { !$0.isDisabled && $1.isDisabled }
         
@@ -50,8 +61,8 @@ class SwapCoordinator: BaseCoordinator, SwapRoutes, SwapInfoRoutes {
     func showPinInput(callback: ((_ pin: String?) -> Void)?) {
         guard let keyStore = try? KeyStore.create() else { return }
         let vc = LoginViewController(for: .confirmation,
-                                     keyMaster: keyStore,
-                                     shouldDisableBiometrics: true)
+                                        keyMaster: keyStore,
+                                        shouldDisableBiometrics: true)
         
         let nvc = RootNavigationController(rootViewController: vc)
         vc.confirmationCallback = { pin in
@@ -78,10 +89,10 @@ class SwapCoordinator: BaseCoordinator, SwapRoutes, SwapInfoRoutes {
         }
     }
     
-    func isDisabledAsset(code: String?, currencies: [Currency]?) -> Bool? {
-        guard let assetCode = code?.uppercased() else { return false }
+    func isDisabledAsset(code: String?, supportedCurrencies: [SupportedCurrency]?) -> Bool? {
+        guard let assetCode = code else { return false }
         
-        return !(currencies?.contains(where: { $0.code == assetCode}) ?? false)
+        return !(supportedCurrencies?.contains(where: { $0.name == assetCode}) ?? false)
     }
     
     // MARK: - Aditional helpers
