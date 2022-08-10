04bda0ddfe288128391cc21395fd47179ef6162d
Kenan Mamedoff <kenan.mamedoff@equaleyes.com>
2022-08-10 13:41:08 +0200

    chore: cleanup

--- brd-ios/breadwallet/src/Redesign/Scenes/Buy/Main/BuyPresenter.swift
+++ brd-ios/breadwallet/src/Redesign/Scenes/Buy/Main/BuyPresenter.swift
@@ -75,18 +75,3 @@ final class BuyPresenter: NSObject, Presenter, BuyActionResponses {
     // MARK: - Additional Helpers
 
 }
-
-struct CardDetailsFormatter {
-    static func formatNumber(last4: String) -> String {
-        return "**** **** **** \(last4)"
-    }
-    
-    static func formatExpirationDate(month: Int, year: Int) -> String {
-        let formattedMonth = String(format: "%02d", month)
-        
-        let year = String(year)
-        let formattedYear: String = year.count == 4 ? String(year.dropFirst(2)) : year
-        
-        return "\(formattedMonth)/\(formattedYear)"
-    }
-}
