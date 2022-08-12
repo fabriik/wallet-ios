65e2029581be624ceba8f19d60040f5a58efbb70
rcresnik <rok.cresnik@equaleyes.com>
2022-08-12 14:52:58 +0200

    fix: add proper asset.

--- brd-ios/breadwallet/src/Redesign/Scenes/Swap/Validation/FailureViewController.swift
+++ brd-ios/breadwallet/src/Redesign/Scenes/Swap/Validation/FailureViewController.swift
@@ -15,7 +15,7 @@ extension Scenes {
 }
 
 class FailureViewController: BaseInfoViewController {
-    override var imageName: String? { return "errorIcon" }
+    override var imageName: String? { return "failedStatusIcon" }
     override var titleText: String? { return "There was an error while processing your payment" }
     override var descriptionText: String? { return "Please contact your card issuer/bank or try again with a different payment method." }
     override var buttonViewModels: [ButtonViewModel] {
@@ -30,20 +30,6 @@ class FailureViewController: BaseInfoViewController {
             goHome,
             showDetails
         ]
-    }
-    
-    override func tableView(_ tableView: UITableView, coverCellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = super.tableView(tableView, coverCellForRowAt: indexPath)
-        
-        guard let cell = cell as? WrapperTableViewCell<FEImageView> else {
-            return cell
-        }
-        cell.wrappedView.snp.makeConstraints { make in
-            make.edges.equalTo(cell.contentView.snp.margins)
-            make.height.equalTo(ViewSizes.large.rawValue)
-        }
-        
-        return cell
     }
 
     func goHome() {
