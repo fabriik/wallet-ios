3e343f59b5618bf1cad18eac5025bd1cd0ef6be2
Kenan Mamedoff <kenan.mamedoff@equaleyes.com>
2022-08-12 11:29:15 +0200

    chore: cleanup

--- brd-ios/breadwallet/src/Redesign/Views/SwapCurrencyView.swift
+++ brd-ios/breadwallet/src/Redesign/Views/SwapCurrencyView.swift
@@ -271,12 +271,12 @@ class SwapCurrencyView: FEView<SwapCurrencyConfiguration, SwapCurrencyViewModel>
     }
     
     @objc func fiatAmountDidChange(_ textField: UITextField) {
-        let text = textField.text?.isEmpty == true ? "0" : textField.text
+        let text = textField.text?.isEmpty != false ? "0" : textField.text
         didChangeFiatAmount?(text)
     }
     
     @objc func cryptoAmountDidChange(_ textField: UITextField) {
-        let text = textField.text?.isEmpty == true ? "0" : textField.text
+        let text = textField.text?.isEmpty != false ? "0" : textField.text
         didChangeCryptoAmount?(text)
     }
     
