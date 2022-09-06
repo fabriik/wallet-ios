5431355ce773ab0318207412c5653fb0f388d0f0
rcresnik <rok.cresnik@equaleyes.com>
2022-09-06 14:24:07 +0200

    chore: extract extension.

---
+++ brd-ios/breadwallet/src/Redesign/Models/Amount+inits.swift
@@ -0,0 +1,60 @@
+// 
+//  Amount+inits.swift
+//  breadwallet
+//
+//  Created by Rok on 06/09/2022.
+//  Copyright © 2022 Fabriik Exchange, LLC. All rights reserved.
+//
+//  See the LICENSE file at the project root for license information.
+//
+
+import UIKit
+import WalletKit
+
+extension Amount {
+    init(amount: Decimal, currency: Currency, exchangeRate: Decimal? = nil, decimals: Int = 9) {
+        let formatter = ExchangeFormatter.crypto
+        let amountString = formatter.string(for: amount) ?? ""
+        
+        guard let exchangeRate = exchangeRate,
+              decimals > 0
+        else {
+            self = .init(tokenString: amountString, currency: currency)
+            return
+        }
+        
+        let rate = Rate(code: currency.code,
+                        name: currency.name,
+                        rate: exchangeRate.doubleValue,
+                        reciprocalCode: "")
+        let value = Amount(tokenString: amountString, currency: currency, rate: rate)
+        guard value.tokenValue > 0 else {
+            self = .init(amount: amount, currency: currency, exchangeRate: exchangeRate, decimals: decimals - 1)
+            return
+        }
+        self = value
+    }
+    
+    init(fiat: Decimal, currency: Currency, exchangeRate: Decimal? = nil, decimals: Int = 9) {
+        let formatter = ExchangeFormatter.fiat
+        let amountString = formatter.string(for: fiat) ?? ""
+        
+        guard let exchangeRate = exchangeRate,
+              decimals > 0
+        else {
+            self = .zero(currency)
+            return
+        }
+        
+        let rate = Rate(code: currency.code,
+                        name: currency.name,
+                        rate: exchangeRate.doubleValue,
+                        reciprocalCode: "")
+        guard let value = Amount(fiatString: amountString, currency: currency, rate: rate),
+              value.tokenValue > 0 else {
+            self = .init(fiat: fiat, currency: currency, exchangeRate: exchangeRate, decimals: decimals - 1)
+            return
+        }
+        self = value
+    }
+}
