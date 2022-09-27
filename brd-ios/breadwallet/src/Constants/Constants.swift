e7d4d42b992f0a8dcf2ecd40bdf86a2616a7d944
Kenan Mamedoff <kenan.mamedoff@equaleyes.com>
2022-09-27 11:45:53 +0400

    chore: cleanup

--- brd-ios/breadwallet/src/Constants/Constants.swift
+++ brd-ios/breadwallet/src/Constants/Constants.swift
@@ -11,45 +11,19 @@ import WalletKit
 
 public enum EventContext: String {
     case none
-    case test
     case onboarding
     case generateKey
     case writeKey
-    case rewards
-    case pushNotifications
-    case inAppNotifications
-    case wallet
-    case jailbreak
-    case fastSync
     case recoverCloud
-    case gift
+    
     var name: String { return rawValue }
 }
 
 public enum Screen: String {
-    case none = ""
-    
-    // general screens
-    case setPin
+    case none
+    case confirmPaperKey
     case paperKeyIntro
     case writePaperKey
-    case confirmPaperKey
-    
-    // onboarding screens
-    case landingPage
-    case globePage
-    case coinsPage
-    case finalPage
-    
-    // push notifications
-    case optInPrompt
-    case systemPrompt
-    case pushNotificationSettings
-    
-    // in-app notifications
-    case inAppNotification
-    
-    case test
     
     var name: String { return rawValue }
 }
