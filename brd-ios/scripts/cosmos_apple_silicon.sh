40605b2e79b4916c9df66b8183d546aacdf5393d
rcresnik <rok.cresnik@equaleyes.com>
2022-09-29 11:31:29 +0200

    chore: add arm/intel check.

--- brd-ios/scripts/cosmos_apple_silicon.sh
+++ brd-ios/scripts/cosmos_apple_silicon.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-# if [[ $(uname -m) != 'arm64' ]]; then
-#  echo "running on intel"
-#  exit 0
-# fi
-# echo "running on arm"
+if [[ $(uname -m) != 'arm64' ]]; then
+ echo "running on intel"
+ exit 0
+fi
+echo "running on arm"
 
 ARM="ios-arm64"
 COSMOS="Cosmos"
